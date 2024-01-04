f0b2a7f3c1011d905015e817beeecb684e098b83
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-04 19:09:11 -0300

    OrderService

--- project (1)/project/src/main/java/com/gabriel/project/repositories/OrderRepository.java
+++ project (1)/project/src/main/java/com/gabriel/project/repositories/OrderRepository.java
@@ -1,6 +1,9 @@
 package com.gabriel.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository {
+
 }
---
+++ project (1)/project/src/main/java/com/gabriel/project/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.gabriel.project.services;
+
+import com.gabriel.project.entities.Order;
+import com.gabriel.project.entities.RequestPerson;
+import com.gabriel.project.repositories.OrderRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository; // Component registration
+
+    public List<Order> getAllOrders() {
+        return repository.findAll();
+    }
+
+    public Optional<Order> findById(UUID id) {
+        return repository.findById(id);
+    }
+}
