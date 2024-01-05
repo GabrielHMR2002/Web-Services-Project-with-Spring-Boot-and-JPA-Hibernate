cc9938b0dd19cb50d2af5e06075e3231cafefc78
gabriel <gabriel.h.miranda.rodrigues@gmail.com>
2024-01-05 00:46:04 -0300

    class Product and Updating class Category

--- project (1)/project/src/main/java/com/gabriel/project/entities/Product.java
+++ project (1)/project/src/main/java/com/gabriel/project/entities/Product.java
@@ -9,23 +9,21 @@ import java.util.Set;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tb_category")
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "tb_product")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
+public class Product implements Serializable {
 
-public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
     private String name;
-
-    private Set<Product> products = new HashSet<>();
-
-
+    private String description;
+    private  Double price;
+    private String imgUrl;
+    private Set<Category> categories = new HashSet<>();
 }
